4d46361bc71d3c5a299d866852bf4dfe612e7609
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-03-23 13:23:00 -0700

    Assignment done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    // TODO
+    int num, max;
+    for(int i = 0; i< 10; i++) {
+        cin >> num;
+
+        if(i==0 || num> max)
+        max = num;
+    }
+    cout <<"Max: " << max << endl;
 
     return 0;
 }
